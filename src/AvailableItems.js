c02b9910afa5b6e77a206848e38f29d841b345d1
Lucas <lucasalonso11800@gmail.com>
2021-06-04 12:54:23 -0300

    Available items

---
+++ src/AvailableItems.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import { cartState } from './state';
+
+function AvailableItems({ inventory }) {
+    const [cart, setCart] = useRecoilState(cartState);
+
+    return (
+        <div className="container">
+            <h2 className="title">Available Items</h2>
+            <pre>{JSON.stringify(cart, null, 2)}</pre>
+            <ul>
+                {Object.entries(inventory).map(([id, { name, price }]) => {
+                    return (
+                        <li key={id}>
+                            {name} ${price.toFixed(2)}
+                            <button onClick={() => setCart({ ...cart, [id]: (cart[id] || 0) + 1 })}>Add</button>
+                            {cart[id] ? <button onClick={() => {
+                                const copy = { ...cart };
+                                if (copy[id] === 1) {
+                                    delete copy[id];
+                                    setCart(copy)
+                                }
+                                else {
+                                    setCart({ ...copy, [id]: copy[id] - 1 })
+                                }
+                            }}>Remove</button> : null}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+};
+
+export default AvailableItems
